bbef5de0fb8040c9db6fd9b9a9e9c48ce33fdce8
klapierre <lapierre.kimberly@gmail.com>
2016-10-26 08:30:06 -0700

    Correct for multiple tests. Change color scheme of heatmap.

--- La Pierre_invasive shrub_nodulation assay_2015.R
+++ La Pierre_invasive shrub_nodulation assay_2015.R
@@ -48,12 +48,15 @@ barGraphStats <- function(data, variable, byFactorNames) {
 #source data management code
 source('Invasive-Shrub_nodulation-assay\\La Pierre_invasive shrub_nodulation assay_2015_data management.R')
 
+#bonferroni correction for multiple tests (3 tests, so corrected by 3)
+bonferroniCorrection <- 1-0.05/3
+
 #do conspecifics nodulate more than allospecifics? do natives nodulate more than invasives?
 summary(conspecificPlantStatusModel <- glm(nod_binary ~ plant_status + is_conspecific, data=nodBinary, family=binomial))
 
   #get confidence intervals around plant status and conspecific traits
-  print(conspecificCI <- exp(confint(conspecificPlantStatusModel, 'is_conspecificTRUE')))
-  print(plantStatusCI <- exp(confint(conspecificPlantStatusModel, 'plant_statusnative')))
+  print(conspecificCI <- exp(confint(conspecificPlantStatusModel, 'is_conspecificTRUE', level=bonferroniCorrection)))
+  print(plantStatusCI <- exp(confint(conspecificPlantStatusModel, 'plant_statusnative', level=bonferroniCorrection)))
 
 #is there an interactions between plant status and rhizobial status (con vs allospecific) in nodulation success?
 #test for interaction with nested model chi-squared test
@@ -64,9 +67,8 @@ print(conspecificPlantStatusInteractionPvalue <- anova(conspecificPlantStatusMod
 #create table of the numbers of strains that are allospecific nodulators for natives and invasives
 allospecificTable <- xtabs(~ plant_status + host_match_factor + nod_binary, data=droplevels(filter(nodBinary, host_match %in% c('native', 'invader'))))
 #fisher test for native species
-print(nativeTest <- fisher.test(allospecificTable['native',,]))
-print(invasiveTest <- fisher.test(allospecificTable['invasive',,][c(2,1),]))
-
+print(nativeTest <- fisher.test(allospecificTable['native',,], conf.level=bonferroniCorrection))
+print(invasiveTest <- fisher.test(allospecificTable['invasive',,][c(2,1),], conf.level=bonferroniCorrection))
 
 
 
@@ -101,6 +103,7 @@ heatmapNodulation <- nodBinary%>%
   spread(plant, nod_binary)
 heatmapNodulation <- heatmapNodulation[order(heatmapNodulation$original_status, heatmapNodulation$original_host),]%>%
   select(strain_label, ACGL, ACWR, GEMO, LUAR, SPJU, ULEU)
+heatmapNodulation <- heatmapNodulation[c('strain_label', 'ACGL', 'ACWR', 'LUAR', 'GEMO', 'SPJU', 'ULEU')]
 
 heatmapNodulation[is.na(heatmapNodulation)] <- -1
 
@@ -116,9 +119,9 @@ reverseNAdist <- function(x,method) {
 binaryHeatmapMatrix <- heatmapMatrix
 binaryColors <- c('white', 'grey', 'black')
 binaryBreaks <- c(-2,-0.5,0.5,2)
-heatmap(binaryHeatmapMatrix, Rowv=NA, margins=c(5,18), breaks=binaryBreaks, col=binaryColors, scale='none',
-        RowSideColors=ifelse(bacteriaNames[rownames(heatmapMatrix)]=='native', '#595959', '#A2A2A2'),
-        ColSideColors=ifelse(plantNames[colnames(heatmapMatrix)]=='native', '#595959', '#A2A2A2'),
+heatmap(binaryHeatmapMatrix, Rowv=NA, Colv=NA, margins=c(5,18), breaks=binaryBreaks, col=binaryColors, scale='none',
+        RowSideColors=ifelse(bacteriaNames[rownames(heatmapMatrix)]=='native', '#009900', '#FF9900'),
+        ColSideColors=ifelse(plantNames[colnames(heatmapMatrix)]=='native', '#009900', '#FF9900'),
         distfun=function(x) reverseNAdist(x, method='binary'))
 legend('topright',
        legend=c('N/A', 'No', 'Yes'),
@@ -128,22 +131,6 @@ legend('topright',
 legend('right',
       legend=c('Native', 'Invasive'),
       title='Plant Status and\nRhizobia Origin',
-      fill=c('#595959', '#A2A2A2'),
+      fill=c('#009900', '#FF9900'),
       bty='n')
 #export at 1200x800 and edit strain labels and rearrange components to make more clear
-
-#boxplot of probability of nodulation by plant status
-ggplot(data=barGraphStats(data=predProbInfo, variable='pred_prob', byFactorNames=c('plant_status', 'plant', 'host_match')), aes(x=plant_status, y=mean, fill=host_match, label=plant)) +
-  geom_boxplot() +
-  # geom_dotplot(binaxis='y', stackdir='center', dotsize=1) +
-  # geom_text(hjust='left', vjust='center', nudge_x=0.05, size=6) +
-  scale_x_discrete(limits=c('native', 'invasive')) +
-  scale_y_continuous(breaks=seq(0.5, 1, 0.1), name="Probability of Nodulation") +
-  scale_fill_manual(name='Isolate Origin',
-                    breaks=c('invader', 'native', 'original'),
-                    labels=c('invasive allospecific', 'native allospecific', 'conspecific'),
-                    values=c("#636363", "#bdbdbd", "#FFFFFF")) +
-  coord_cartesian(ylim=c(0.45, 1)) +
-  xlab("Test-Host Status") +
-  theme(legend.justification=c(0,0), legend.position=c(0,0))
-#export at 700x700
